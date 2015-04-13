21cce16514cd07a0d382774457d7bd2c422b4de8
Chaosviper <ricky.stucchi@gmail.com>
2015-04-13 23:48:08 +0200

    Added Node implementation

--- AIProgramming/Node.cpp
+++ AIProgramming/Node.cpp
@@ -1,3 +1,49 @@
 #include "Node.h"
+#include "NodeState.h"
 
 
+Node::Node(){
+	// Create a standard configuration that allow at least one solution (always the same)
+	configuration[0] = 13;
+	configuration[1] = 5;
+	configuration[2] = 14;
+	configuration[3] = 11;
+	configuration[4] = 12;
+	configuration[5] = 15;
+	configuration[6] = 7;
+	configuration[7] = 2;
+	configuration[8] = 1;
+	configuration[9] = 10;
+	configuration[10] = 3;
+	configuration[11] = 9;
+	configuration[12] = 8;
+	configuration[13] = 6;
+	configuration[14] = 4;
+	configuration[15] = 16; // 16 rapresent the hole!!
+
+	state = NodeState::Unknown;
+}
+
+
+Node::Node(const Node& nodeBase){
+	CopyConfigurationFrom(nodeBase);
+
+	state = NodeState::Unknown;
+}
+
+Node::~Node(){
+
+}
+
+void Node::CopyConfigurationFrom(const Node& toCopy){
+	memcpy(configuration, toCopy.configuration, NODE_MAX_ELEM_STATUS);
+}
+
+bool Node::operator==(const Node& toCheck) const{
+	
+	for (int i = 0; i < NODE_MAX_ELEM_STATUS; ++i)
+		if (configuration[i] != toCheck.configuration[i])
+			return false;
+
+	return true;
+};
--- AIProgramming/Node.h
+++ AIProgramming/Node.h
@@ -1,8 +1,31 @@
 #pragma once
+#include <list>
 
-class Node{
-	public:
-		Node();
-		~Node();
-	private:
+#define NODE_MAX_ELEM_STATUS 15
+
+enum NodeState; // <-- Forward Declaration
+
+struct Node{
+
+	// ** VARIABLES **
+
+	Node* parent;
+	NodeState state;
+	std::list<Node*> adjNodes;
+
+	int g;
+	int h;
+	int f;
+
+	short configuration[NODE_MAX_ELEM_STATUS];
+
+	// ** FUNCTIONS **
+
+	Node();
+	Node(const Node&);
+	~Node();
+		
+	void CopyConfigurationFrom(const Node&);
+	bool operator==(const Node&) const;
+
 };
