7b2e4e793295b2dba83326a1b0ead7cf4c1b8312
Fuma13 <Fuma13@users.noreply.github.com>
2015-11-14 09:35:11 +0100

    MainMarco

--- FMSSteering/src/Main/MainMarco.cpp
+++ FMSSteering/src/Main/MainMarco.cpp
@@ -1,22 +1,31 @@
 #include "NanoBehaviour\NanoAgent.h"
-#include "NanoBehaviour\NanoFSMCore.h"
-#include "FiniteStateMachine.h"
+#include "SheepBehaviour\SheepAgent.h"
+#include "DogBehaviour\DogAgent.h"
+
+#include "PassiveObj\Fence.h"
+#include "PassiveObj\Home.h"
+#include "PassiveObj\Mine.h"
 
 #include "ViewComponent.h"
 
-void InitMarco(){
-	//FiniteStateMachine<NanoAgent> x;
-	//
-	//NanoAgent newNano(10,100);
-	//ViewComponent* viewNano = new ViewComponent(ViewManager::DWARF, newNano.GetPosition(), 0);
-	//newNano.SetViewComponent(viewNano);
+NanoAgent newNano("Nano", 100, 10, 5);
+SheepAgent sheep("Sheep");
+DogAgent dog("Dog");
 
-	////FSMCore<NanoAgent>& x = FSMCore<NanoAgent>::GetInstance();
+Mine mine(100);
+Home home(1);
+Fence fence(MagicNumber::POS_BACKGROUND_FENCE, MagicNumber::FENCE_SIZE);
+
+void InitMarco(){
+
 }
 
 void StartMarco(){
 }
 
 void RunMarco(){
+	newNano.FSMRun(MagicNumber::deltaTime);
+	sheep.FSMRun(MagicNumber::deltaTime);
+	dog.FSMRun(MagicNumber::deltaTime);
 
 }
